ae63ad3313d991faf105acf5dee80cc0e10bf5b1
Oscar Fabra <oscarfabra@gmail.com>
2020-04-07 16:56:02 -0500

    Adds Object example

--- linkedin-js-et/script.js
+++ linkedin-js-et/script.js
@@ -1,14 +1,22 @@
-// const MYCONSTANT = 5;
-// console.log(MYCONSTANT);
-// MYCONSTANT = 6;
+var course = new Object();
 
-function logScope() {
-  var localVar = 2;
-  if(localVar) {
-    let localVar = "I'm different!";
-    console.log("nested localVar: ", localVar);
+// course.title = "JavaScript Essential Training";
+// course.instructor = "Morten Rand-Hendriksen";
+// course.level = 1;
+// course.published = true;
+// course.views = 0;
+
+var course = {
+  title: "JavaScript Essential Training",
+  instructor: "Morten Rand-Hendriksen",
+  level: 1,
+  published: true,
+  views: 0,
+  updateViews: function() {
+    return ++course.views;
   }
-  console.log("logScope localVar: ", localVar);
 }
 
-logScope();
+console.log(course.views);
+course.updateViews();
+console.log(course.views);
