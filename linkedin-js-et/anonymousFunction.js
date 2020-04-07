a3d70f58da444088d404a02beae6c38701530b8a
Oscar Fabra <oscarfabra@gmail.com>
2020-04-07 16:02:20 -0500

    Puts anonymous function in its own file

---
+++ linkedin-js-et/anonymousFunction.js
@@ -0,0 +1,13 @@
+// var a = 5 / 7;
+// var b = 18 / 25;
+
+var theBiggest = function(a, b) {
+  var result;
+  a > b ? result = ["a", a] : result = ["b", b];
+  return result;
+}
+
+console.log(theBiggest(7 / 9, 13 / 25));
+
+//Returns the function code
+// console.log(theBiggest);
