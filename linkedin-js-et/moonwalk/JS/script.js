76e0e56293c6973daa7ac4d9e8abfe81ec56ca0b
Oscar Fabra <oscarfabra@gmail.com>
2020-04-08 17:19:17 -0500

    Adds moonwalk.html document and script to append figcaption

--- linkedin-js-et/moonwalk/JS/script.js
+++ linkedin-js-et/moonwalk/JS/script.js
@@ -1,9 +1,30 @@
-const CTAELEMENT = document.querySelector(".cta a");
+// const CTAELEMENT = document.querySelector(".cta a");
 
-if(CTAELEMENT.hasAttribute("target")) {
-  console.log(CTAELEMENT.getAttribute("target"));
-} else {
-  CTAELEMENT.setAttribute("target", "_blank");
-}
+// if(CTAELEMENT.hasAttribute("target")) {
+//   console.log(CTAELEMENT.getAttribute("target"));
+// } else {
+//   CTAELEMENT.setAttribute("target", "_blank");
+// }
 
-console.log(CTAELEMENT.attributes);
+// console.log(CTAELEMENT.attributes);
+
+// 0. Gets alt text
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+var altText = THEIMAGE.getAttribute("alt");
+// 1. Creates figcaption element
+var captionElement = document.createElement("figcaption");
+// // 2. Creates text node with alt text
+// var captionText = document.createTextNode(altText);
+// // 3. Inserts text node inside figcaption element
+// captionElement.appendChild(captionText);
+// // 4. Inserts figcaption element inside featured-image
+// FEATURED.appendChild(captionElement);
+
+// Simpler method for newer browsers
+captionElement.append(altText);
+FEATURED.append(captionElement);
+
+THEIMAGE.setAttribute("alt", "");
+
+console.log(captionElement);
